1b60483bd6b6ac6a959168889332f8587f441f88
Andrew Eisenberg <aeisenberg@github.com>
2025-01-28 16:18:06 -0800

    New Stuff

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ import yahooFinance from 'yahoo-finance2';
 // Function to get stock prices
 async function getStockPrices(stockTicker) {
     try {
-        const options = { period1: '7d', interval: '1d' }; //Fetches last weeks prices
-        const prices = await yahooFinance.historical(stockTicker, options);
-        console.log(`Prices for ${stockTicker} over the last week`);
-        prices.forEach((price) => {
+        // const options = { period1: '7', interval: '1d' }; //Fetches last weeks prices
+        // const prices = await yahooFinance.historical(stockTicker, options);
+        // console.log(`Prices for ${stockTicker} over the last week`);
+        // prices.forEach((price) => {
+        //     console.log(`Date: ${price.date}, Close: ${price.close}`);
+        // });
+
+        // https://github.com/gadicc/node-yahoo-finance2/blob/devel/docs/modules/chart.md
+        const queryOptions = { period1: '2021-05-08', interval: '1d' };
+        const result = await yahooFinance.chart(stockTicker, queryOptions);
+        result.quotes.forEach((price) => {
             console.log(`Date: ${price.date}, Close: ${price.close}`);
         });
+        console.log(result.events.dividends);
+        console.log(result.meta);
+
+        console.log(Object.keys(result));
     } catch (error) {
         console.error(`Sorry, we couldn't find data for ${stockTicker}.`);
+        console.error(error);
     }
 }
 
